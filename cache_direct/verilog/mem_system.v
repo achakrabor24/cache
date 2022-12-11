cf87e8a2c0d9f5806b7f5a2e1c34a74f136a6a40
achakrabor24 <71959953+achakrabor24@users.noreply.github.com>
2022-12-11 15:28:52 -0600

    The cache that runs

--- cache_direct/verilog/mem_system.v
+++ cache_direct/verilog/mem_system.v
@@ -27,23 +27,24 @@ module mem_system(/*AUTOARG*/
     * needed for cache parameter */
     parameter memtype = 0;
 
-
-    // Internal signals
-    wire write_cache, valid_in, ch, comp, done, enable, stall, stall_mem, write_mem, read_mem, errCache, errMem, errCtrl, hit, dirty, valid;
-    wire [4:0] tag_out, tag_in, tag_mem;
-    wire [7:0] index;
-    wire [2:0] offset, offset_mem;
-    wire [15:0] data_in, data_in_mem, data_from_cache, data_out_mem, addr_mem;
-    wire [3:0] busy_mem;
-
+    wire hit, dirty, valid;
+    reg write_c, valid_in, ch, comp, done, enable;
+    reg wr_m, rd_m;
+    wire [4:0] tag_out;
+    reg [4:0]tag_in, tag_m;
+    reg [7:0] index;
+    reg [2:0] offset;
+    reg [15:0] data_in, data_in_m;
+    wire [15:0] data_out, data_out_m, addr_mem;
+    wire err_c, err_m;
 
     cache #(0 + memtype) c0(// Outputs
                           .tag_out              (tag_out),
-                          .data_out             (data_from_cache),
+                          .data_out             (data_out),
                           .hit                  (hit),
                           .dirty                (dirty),
                           .valid                (valid),
-                          .err                  (errCache),
+                          .err                  (err_c),
                           // Inputs
                           .enable               (enable),
                           .clk                  (clk),
@@ -54,54 +55,274 @@ module mem_system(/*AUTOARG*/
                           .offset               (offset),
                           .data_in              (data_in),
                           .comp                 (comp),
-                          .write                (write_cache),
+                          .write                (write_c),
                           .valid_in             (valid_in));
 
+    wire stall_m;
+    wire [3:0] busy_mem;
 
-    four_bank_mem fb0(// Outputs
-                     .data_out          (data_out_mem),
-                     .stall             (stall_mem),
+    four_bank_mem mem(// Outputs
+                     .data_out          (data_out_m),
+                     .stall             (stall_m),
                      .busy              (busy_mem),
-                     .err               (errMem),
+                     .err               (err_m),
                      // Inputs
                      .clk               (clk),
                      .rst               (rst),
                      .createdump        (createdump),
                      .addr              (addr_mem),
-                     .data_in           (data_in_mem),
-                     .wr                (write_mem),
-                     .rd                (read_mem));
+                     .data_in           (data_in_m),
+                     .wr                (wr_m),
+                     .rd                (rd_m));
 
 
+    // your code here
 
-assign addr_mem = {tag_mem, index, offset_mem};
+    reg err_s;
+    assign err = err_m | err_c | err_s;
+    wire [5:0] state;
+    reg [5:0] next_state;
 
-cache_controller cc(
-// Outputs
-.comp(comp), .valid_in(valid_in), .cache_hit(CacheHit), .done(Done), .wr_m(write_mem), .rd_m(read_mem), .errCtrl(errCtrl), 
-.write_c(write_cache), .enable(enable), .index(index), .offset(offset), .tag_in(tag_in), 
-.stall(stall), .data_in_m(data_in_mem), .tag_m(tag_mem), .offset_m(offset_mem), .data_in(data_in),
+    wire miss;
+    assign miss = ~hit | (hit & ~valid);
+    assign Done = done;
 
-// Inputs
-.addr(Addr), .data_out(data_from_cache), .data_input(DataIn), .hit(hit), .valid(valid), .dirty(dirty), .Rd(Rd), .Wr(Wr), .tag_out(tag_out), 
-.data_out_m(data_out_mem));
+    dff i_dff[5:0] (.q(state), .d(next_state), .clk(clk), .rst(rst));
 
-assign err = errMem | errCache | errCtrl;
+    parameter IDLE = 6'h00;
 
-assign DataOut = data_from_cache;
+    parameter compRead  = 6'd1;
+    parameter compWrite = 6'd2;
 
-wire [5:0] state;
-reg [5:0] next_state;
+    parameter accessRead0 = 6'd3;
+    parameter accessRead1 = 6'd4;
+    parameter accessRead2 = 6'd5;
+    parameter accessRead3 = 6'd6;
 
-dff dff0[5:0](.q(state), .d(next_state), .clk(clk), .rst(rst));
+    parameter WB_0 = 6'd7;
+    parameter WB_1 = 6'd8;
+    parameter WB_2 = 6'd9;
+    parameter WB_3 = 6'd10;
 
-// How to with output wire type and in this module reg type (and to pass vcheck)
-always @* begin
-	case(state) 
-		default: Stall = stall;
-	endcase
-end
+    parameter memRead0 = 6'd11;
+    parameter memRead1 = 6'd12;
+    parameter memRead2 = 6'd13;
+    parameter memRead3 = 6'd14;
 
+    parameter accessWrite0 = 6'd15;
+    parameter accessWrite1 = 6'd16;
+    parameter accessWrite2 = 6'd17;
+    parameter accessWrite3 = 6'd18;
+
+    parameter wb_xxx     = 6'd29;
+    parameter writeCache = 6'd30;
+    parameter DONE = 6'd31;
+
+    parameter wait_0_0 = 6'd19;
+    parameter wait_0_1 = 6'd20;
+
+    parameter wait_1_0 = 6'd21;
+    parameter wait_1_1 = 6'd22;
+
+    parameter wait_2_0 = 6'd23;
+    parameter wait_2_1 = 6'd24;
+
+    parameter wait_3_0 = 6'd25;
+    parameter wait_3_1 = 6'd26;
+
+    parameter wait_4_0 = 6'd27;
+    parameter wait_4_1 = 6'd28;
+
+    parameter ERROR = 6'd50;
+
+    reg [2:0] offset_m;
+
+    assign CacheHit = ch;
+    assign addr_mem = {tag_m, Addr[10:3], offset_m};
+    assign DataOut = data_out;
+
+    always @(*) begin
+
+        next_state = state;
+        valid_in = 1'b0;
+        err_s = 1'b0;
+        done = 1'b0;
+        comp = 1'b0;
+        write_c = 1'b0;
+        enable = 1'b0;
+        tag_in = Addr[15:11];
+        index = Addr[10:3];
+        offset = Addr[2:0];
+        offset_m = 3'd0;
+        Stall = 1'b1;
+        tag_m = Addr[15:11];
+        wr_m = 1'b0;
+        rd_m = 1'b0;
+        data_in_m = data_out;
+        data_in = DataIn;
+        ch = 1'b0;
+
+      case(state)
+        IDLE: begin
+            enable = 1'b1;
+            Stall = 1'b0;
+            next_state = (Rd & Wr) ? ERROR :
+                       (Rd & ~Wr) ? compRead :
+                       (~Rd & Wr) ? compWrite : IDLE;
+        end
+        compRead: begin
+            enable = 1'b1;
+            comp = 1'b1;
+            next_state = (hit & valid) ? IDLE :
+                       (miss) ? (dirty) ? accessRead0 : accessWrite0 : ERROR;
+            done = (hit & valid) ? 1'b1 : 1'b0;
+            ch = (hit & valid) ? 1'b1 : 1'b0;
+        end
+        compWrite: begin
+            enable = 1'b1;
+            write_c = 1'b1;
+            comp = 1'b1;
+            next_state = (hit & valid) ? IDLE :
+                         (miss) ? (dirty) ? accessRead0 : accessWrite0 : ERROR;
+            done = (hit & valid) ? 1'b1 : 1'b0;
+            ch = (hit & valid);
+        end
+        accessRead0: begin
+            // store the cache value in mem after 4 cycles
+            enable = 1'b1;
+            offset = 3'd0;
+            tag_m = (valid) ? tag_out : Addr[15:11];
+            data_in_m = data_out;
+            wr_m = 1'b1;
+            offset_m = 3'b00_0;
+            next_state = accessRead1;
+        end
+        accessRead1: begin
+            enable = 1'b1;
+            offset = 3'b01_0;
+            tag_m = (valid) ? tag_out : Addr[15:11];
+            data_in_m = data_out;
+            wr_m = 1'b1;
+            offset_m = 3'b01_0;
+            next_state = accessRead2;
+        end
+        accessRead2: begin
+            enable = 1'b1;
+            offset = 3'b10_0;
+            tag_m = (valid) ? tag_out : Addr[15:11];
+            data_in_m = data_out;
+            wr_m = 1'b1;
+            offset_m = 3'b10_0;
+            next_state = accessRead3;
+        end
+        accessRead3: begin
+            enable = 1'b1;
+            offset = 3'b11_0;
+            tag_m = (valid) ? tag_out : Addr[15:11];
+            data_in_m = data_out;
+            wr_m = 1'b1;
+            offset_m = 3'b11_0;
+            next_state = wait_0_0;
+        end
+        wait_0_0: begin
+            next_state = wait_0_1;
+        end
+        wait_0_1: begin
+            next_state = wait_1_0;
+        end
+        wait_1_0: begin
+            next_state = wait_1_1;
+        end
+        wait_1_1: begin
+            next_state = wait_2_0;
+        end
+        wait_2_0: begin
+            next_state = accessWrite0;
+        end
+        accessWrite0: begin
+            // ask mem for data @Addr, will be available 2 cycles later
+            rd_m = 1'b1;
+            offset_m = 3'b00_0;
+            next_state = accessWrite1;
+        end
+        accessWrite1: begin
+            rd_m = 1'b1;
+            offset_m = 3'b01_0;
+            next_state = accessWrite2;
+        end
+        accessWrite2: begin
+            rd_m = 1'b1;
+            offset_m = 3'b10_0;
+
+            ///////////////////
+            ////   Cache   ////
+            ///////////////////
+            enable = 1'b1;
+            offset = 3'b00_0;
+            write_c = 1'b1;
+            valid_in = 1'b1;
+            data_in = data_out_m;
+
+            next_state = accessWrite3;
+        end
+        accessWrite3: begin
+            rd_m = 1'b1;
+            offset_m = 3'b11_0;
+
+            ///////////////////
+            ////   Cache   ////
+            ///////////////////
+            enable = 1'b1;
+            offset = 3'b01_0;
+            write_c = 1'b1;
+            valid_in = 1'b1;
+            data_in = data_out_m;
+
+            next_state = wait_2_1;
+        end
+        wait_2_1: begin
+            ///////////////////
+            ////   Cache   ////
+            ///////////////////
+            enable = 1'b1;
+            offset = 3'b10_0;
+            write_c = 1'b1;
+            valid_in = 1'b1;
+            data_in = data_out_m;
+            next_state = wait_3_0;
+        end
+        wait_3_0: begin
+            ///////////////////
+            ////   Cache   ////
+            ///////////////////
+            enable = 1'b1;
+            offset = 3'b11_0;
+            write_c = 1'b1;
+            valid_in = 1'b1;
+            data_in = data_out_m;
+            next_state = Wr ? wait_4_0 : wait_3_1;
+        end
+        wait_3_1: begin
+            ///////////////////
+            ////   Cache   ////
+            ///////////////////
+            enable = 1'b1;
+            done = 1'b1;
+            next_state = IDLE;
+        end
+        wait_4_0: begin
+            ///////////////////
+            ////   Cache   ////
+            ///////////////////
+            enable = 1'b1;
+            write_c = 1'b1;
+            comp = 1'b1;
+            valid_in = 1'b1;
+            next_state = wait_3_1;
+        end
+        default: err_s = 1'b1;
+      endcase
+    end
 
 
 endmodule // mem_system
